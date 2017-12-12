86a5c5e514785dceaa1b9ff866cb93725c9bfd39
Huigesi <791339970@qq.com>
2017-12-12 10:33:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/fragmentsdemo/fragment/Fragment_1.java
@@ -0,0 +1,30 @@
+package com.example.administrator.fragmentsdemo.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/12/12.
+ */
+
+public class Fragment_1 extends Fragment{
+    public Fragment_1() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
