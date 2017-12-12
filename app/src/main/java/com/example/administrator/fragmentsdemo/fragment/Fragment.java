bb3cc41030fdcf959e3957fd9d9037020dc82107
Huigesi <791339970@qq.com>
2017-12-12 10:44:25 +0800

    fragment first try

--- app/src/main/java/com/example/administrator/fragmentsdemo/fragment/Fragment.java
+++ app/src/main/java/com/example/administrator/fragmentsdemo/fragment/Fragment.java
@@ -42,7 +42,7 @@ public class Fragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+        return inflater.inflate(R.layout.fragment, container, false);
     }
 
     public void onButtonPressed(Uri uri) {
--- app/src/main/java/com/example/administrator/fragmentsdemo/fragment/Fragment_1.java
+++ app/src/main/java/com/example/administrator/fragmentsdemo/fragment/Fragment_1.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.administrator.fragmentsdemo.R;
+
 /**
  * Created by Administrator on 2017/12/12.
  */
@@ -20,7 +22,9 @@ public class Fragment_1 extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view=inflater.inflate(R.layout.fragment_1,null);
+        return view;
     }
 
     @Override
