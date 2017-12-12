87cfc78038230ad9917b8495ad1e33690f72ff63
Huigesi <791339970@qq.com>
2017-12-12 15:49:23 +0800

    fragment add

--- app/src/main/java/com/example/administrator/fragmentsdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/fragmentsdemo/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.administrator.fragmentsdemo;
-
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.administrator.fragmentsdemo.fragment.Fragment_1;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button_add,button_remove;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //_____-----fragment_add
+        button_add=(Button)findViewById(R.id.btn_show);
+        button_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager=getFragmentManager();
+                fragmentTransaction=fragmentManager.beginTransaction();
+                Fragment_1 fragment_1=new Fragment_1();
+                fragmentTransaction.add(R.id.right,fragment_1);
+                fragmentTransaction.commit();
+            }
+        });
+        //_____-----fragment_remove
+
+
+
+
     }
 }
--- app/src/main/java/com/example/administrator/fragmentsdemo/fragment/Fragment_1.java
+++ app/src/main/java/com/example/administrator/fragmentsdemo/fragment/Fragment_1.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.administrator.fragmentsdemo.R;
 
@@ -23,8 +25,16 @@ public class Fragment_1 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        View view=inflater.inflate(R.layout.fragment_1,null);
+        Button button=(Button)view.findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"fragment_1",Toast.LENGTH_SHORT).show();
+            }
+        });
+        //return inflater.inflate(R.layout.fragment_1, container, false);
+        return view;
     }
 
     @Override
